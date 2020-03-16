c3fdaba63eae5793e47cd8f0a4a85b784832cab1
veroalrodriguez <veroalrodriguez@gmail>
2020-03-15 23:53:27 -0600

    funcionando a-z z-a

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+<img src="z-img-readme/initial.png">
+
+
 # Data Lovers
 
 ## Índice
--- src/data.js
+++ src/data.js
@@ -1,13 +1,40 @@
-// import data from './data/injuries/injuries.js';
-// import data from './data/lol/lol.js';
-// import data from './data/patient/patient.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-// import data from './data/steam/steam.js';
-// import data from './data/steam/worldbank.js';
+//Buscador por nombre
 
-// esta es una función de ejemplo
+//Ordenando
+export const asc = (rickandmorty) => {
+  const array = rickandmorty.sort((object1, object2) => ((object1.name > object2.name) ? 1 : -1));
+  return array;
+};
 
-export const example = () => {
-  return 'example';
+export const des = (rickandmorty) => {
+  const array = rickandmorty.sort((object1, object2) => ((object1.name > object2.name) ? -1 : 1));
+  return array;
+};
+
+//Filtrado por especie
+export const filterSpecies = (rickandmorty, value) => {
+  const array = [];
+
+  rickandmorty.forEach((object) => {
+    object.species.forEach((species) => {
+      if (species === value) {
+        array.push(object);
+      }
+    });
+  });
+  return array;
+};
+
+//Filtrado por estado
+export const filterStatus = (rickandmorty, value) => {
+  const array = [];
+
+  rickandmorty.forEach((object) => {
+    object.status.forEach((status) => {
+      if (status === value){
+        array.push
+      }
+    });
+  });
+  return array;
 };
